3026b09177a8ddac79df9608b92e706a32cdb65e
BoganJohn <74276347+BoganJohn@users.noreply.github.com>
2021-01-13 09:43:36 -0600

    Add files via upload

---
+++ Wk9D2Lab/src/com/tts/Main.java
@@ -0,0 +1,43 @@
+package com.tts;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List <Integer> intList=new ArrayList<Integer>();
+        int sum = 0;
+        int product = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter any series of 5 numbers separated by a space: ");
+        String[] strNums = null;
+        if (scanner.hasNextLine()) {
+            strNums = scanner.nextLine().split(" ");
+        }
+        if (strNums != null) {
+            for (String strNum: strNums) {
+                try {
+                    intList.add(Integer.parseInt(strNum.trim()));
+                } catch (Exception e) {
+                    System.out.println("Invalid input");
+                    break;
+                }
+            }
+
+            Integer[] nums = intList.toArray(new Integer[0]);
+            System.out.println(Arrays.toString(nums));
+            for(int num: nums) {
+                sum = sum + num;
+            }
+            for(int num: nums){
+                product = product * num;
+            }
+        }
+
+        System.out.println("Sum of the elements of the array ::"+sum);
+        System.out.println("Product of the elements of the array ::"+product);
+        }
+
+}
